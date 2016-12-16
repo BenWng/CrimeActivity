f959908134300dae1a53102e156be15f2e9823f1
BenWng <wnghan@gmail.com>
2016-12-16 16:49:42 +0800

    Add Singleton Class CrimeLab

---
+++ app/src/main/java/com/ben/crimeactivity/SingleFragmentActivity.java
@@ -0,0 +1,10 @@
+package com.ben.crimeactivity;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Ben_Big on 12/16/16.
+ */
+
+public class SingleFragmentActivity extends FragmentActivity {
+}
