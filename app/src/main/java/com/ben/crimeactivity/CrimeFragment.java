820b7000bf6a21db9499a9fbe30bfb6fdc3961cd
BenWng <wnghan@gmail.com>
2017-01-05 16:50:25 -0500

    Finish up to page 226

--- app/src/main/java/com/ben/crimeactivity/CrimeFragment.java
+++ app/src/main/java/com/ben/crimeactivity/CrimeFragment.java
@@ -3,6 +3,7 @@ package com.ben.crimeactivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -23,6 +24,7 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE="DialogDate";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -75,7 +77,17 @@ public class CrimeFragment extends Fragment {
 
         mDateButton = (Button) v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                FragmentManager manager=getFragmentManager();
+                DatePickerFragment dialog=DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.show(manager,DIALOG_DATE);
+            }
+        });
+
+
+
 
 
         mSolvedCheckBox=(CheckBox)v.findViewById(R.id.crime_solved);
